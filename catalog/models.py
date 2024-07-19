c964d35c24a369b1a1bebdb8553330c1d4e0472d
Sergei Bykov <serega1988bykov@gmail.com>
2024-07-19 11:05:34 +0500

    add models

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200,
+                            help_text='Введите жанр книги',
+                            verbose_name='Жанр книги')
+
+    def __str__(self):
+        return self.name
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=20,
+                            help_text='Введите язык книги',
+                            verbose_name='Язык книги')
+
+    def __str__(self):
+        return self.name
+
+
+class Published(models.Model):
+    name = models.CharField(max_length=20,
+                            help_text='Введите наименование издательства',
+                            verbose_name='Издательство')
+
+    def __str__(self):
+        return self.name
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100,
+                                  help_text='Введите имя автора',
+                                  verbose_name='Имя автора')
+    last_name = models.CharField(max_length=100,
+                                 help_text='Введите фамилию автора',
+                                 verbose_name='Фамилия автора')
+    date_of_birth = models.DateField(help_text='Введите дату рождения',
+                                     verbose_name='Дата рождения',
+                                     null=True, blank=True)
+    about = models.TextField(help_text='Введите сведения об авторе',
+                             verbose_name='Сведения об авторе')
+    photo = models.ImageField(upload_to='images',
+                              help_text='Введите фото автора',
+                              verbose_name='Фото автора',
+                              null=True, blank=True)
+
+    def __str__(self):
+        return self.last_name
+
