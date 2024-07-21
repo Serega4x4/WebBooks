f8d4d8bdf01dbc7bc2db948e444e630b851d7ee4
Sergei Bykov <serega1988bykov@gmail.com>
2024-07-21 10:09:33 +0500

    add models in db and links

--- catalog/models.py
+++ catalog/models.py
@@ -20,7 +20,7 @@ class Language(models.Model):
         return self.name
 
 
-class Published(models.Model):
+class Publisher(models.Model):
     name = models.CharField(max_length=20,
                             help_text='Введите наименование издательства',
                             verbose_name='Издательство')
@@ -49,3 +49,60 @@ class Author(models.Model):
     def __str__(self):
         return self.last_name
 
+
+class Book(models.Model):
+    title = models.CharField(max_length=200, help_text=';Введите название книги',
+                             verbose_name='Название книги')
+    genre = models.ForeignKey('Genre', on_delete=models.CASCADE,
+                              help_text='Выберите жанр книги',
+                              verbose_name='Жанр книги', null=True)
+    language = models.ForeignKey('Language', on_delete=models.CASCADE,
+                                 help_text='Выберите язык книги',
+                                 verbose_name='Язык книги', null=True)
+    publisher = models.ForeignKey('Publisher', on_delete=models.CASCADE,
+                                  help_text='Выберите издательство',
+                                  verbose_name='Издательство', null=True)
+    year = models.CharField(max_length=4, help_text='Введите год издания',
+                            verbose_name='Год издания')
+    author = models.ManyToManyField('Author', help_text='Выберите автора (авторов) книги',
+                                    verbose_name='Автор (Авторы) книги')
+    summary = models.TextField(max_length=1000, help_text='Введите краткое описание книги',
+                               verbose_name='Аннотация книги')
+    isbn = models.CharField(max_length=13, help_text='Должно содержать 13 символов',
+                            verbose_name='ISBN книги')
+    price = models.DecimalField(decimal_places=2, max_digits=7,
+                                help_text='Введите цену книги',
+                                verbose_name='Цена (руб.)')
+    photo = models.ImageField(upload_to='images', help_text='Введите изображение обложки',
+                              verbose_name='Изображение обложки')
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('book-detail', args=[str(self.id)])
+
+
+class Status(models.Model):
+    name = models.CharField(max_length=20, help_text='Введите статус экземпляра книги',
+                            verbose_name='Статус экземпляра книги')
+
+    def __str__(self):
+        return self.name
+
+
+class BookInstans(models.Model):
+    book = models.ForeignKey('Book', on_delete=models.CASCADE, null=True)
+    inv_nom = models.CharField(max_length=20, null=True, help_text='Введите инвентарный номер экземпляра',
+                               verbose_name='Статус экземпляра книги')
+    status = models.ForeignKey('Status', on_delete=models.CASCADE, null=True,
+                               help_text='Изменить состояние книги',
+                               verbose_name='Статус экземпляра книги')
+    due_back = models.DateField(null=True, blank=True, help_text='Введите конец срока статуса',
+                                verbose_name='Даиа окончания статуса')
+
+    class Meta:
+        ordering = ['due_back']
+
+        def __str__(self):
+            return '%s %s %s % (self.inv_nom, self.book, self.status)'
