c8e3c78543a29d38bdd9003616fca8410e14942b
Sergei Bykov <serega1988bykov@gmail.com>
2024-07-22 19:36:17 +0500

    work wuth admin panel

--- catalog/admin.py
+++ catalog/admin.py
@@ -10,15 +10,26 @@ class AuthorAdmin(admin.ModelAdmin):
 admin.site.register(Author, AuthorAdmin)
 
 
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'genre', 'language', 'display_author')
     list_filter = ('genre', 'author')
+    inlines = [BookInstanceInline]
 
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
     list_filter = ('book', 'status')
+    fieldsets = (
+        ('Экземпляр книги', {
+            'fields': ('book', 'inv_nom')}),
+        ('Статус и кончание его действия', {
+            'fields': ('status', 'due_back')}),
+         )
 
 
 admin.site.register(Genre)
