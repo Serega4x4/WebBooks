998ba6d9051e01f6e113a30d3b60f5ef535cd599
Sergei Bykov <serega1988bykov@gmail.com>
2024-07-28 07:33:15 +0500

    working with admin panel files

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Author, Book, Genre, Language, Publisher, Status, BookInstance
 
 
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('last_name', 'first_name', 'date_of_birth', 'photo')
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'photo', 'show_photo')
     fields = ['last_name', 'first_name', ('date_of_birth', 'photo')]
+    readonly_fields = ['show_photo']
+
+    def show_photo(self, obj):
+        return format_html(
+            f'<img src="{obj.photo.url}" style="max-height: 100px;">')
+    show_photo.short_description = 'Фото'
 
 
 admin.site.register(Author, AuthorAdmin)
@@ -16,9 +23,16 @@ class BookInstanceInline(admin.TabularInline):
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'genre', 'language', 'display_author')
+    list_display = ('title', 'genre', 'language', 'display_author', 'show_photo')
     list_filter = ('genre', 'author')
     inlines = [BookInstanceInline]
+    readonly_fields = ['show_photo']
+
+    def show_photo(self, obj):
+        return format_html(
+            f'<img src="{obj.photo.url}" style="max-height: 100px;">')
+
+    show_photo.short_description = 'Обложка'
 
 
 @admin.register(BookInstance)
