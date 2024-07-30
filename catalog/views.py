29c0e1cf6dfa15e5a269032aca23cfe5051b9458
Sergei Bykov <serega1988bykov@gmail.com>
2024-07-30 21:02:27 +0500

    create temolates & views & urls

--- catalog/views.py
+++ catalog/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Главная страница "Мир Книг"!')
+    text_head = 'Это заголовок главной страницы сайта'
+    text_body = 'Это содержимое страницы сайта'
+    context = {'text_head': text_head, 'text_body': text_body}
+    return render(request, 'catalog/index.html', context)
